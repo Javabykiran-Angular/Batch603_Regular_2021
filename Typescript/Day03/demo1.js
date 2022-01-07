7a9c7cb3cbeffa3edc319d20957e0a1967a11409
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-01-07 10:05:57 +0530

    Day03

---
+++ Typescript/Day03/demo1.js
@@ -0,0 +1,23 @@
+// console.log('Hello World');
+var a1, b1;
+var a$b;
+//data types
+// number(int,long,short,double,float),string("",''),boolean(true/false)
+// any (number,string,boolean), void(netural it does point any thing)
+//misec. null & undefined (value as well as datatype)
+var a = 10; //forward declaration & defination
+// console.log("Value of a is "+a);
+// console.log(`value of a is => ${a} `);
+// var b:number;
+// console.log('Value of b is '+b);
+// var str:string;
+// console.log('Str Value is => '+str)
+// str="s";
+// console.log('Str Value is => '+str);
+var b;
+b = 2.5;
+console.log("Value of b is " + b);
+b = 'Sumit';
+console.log("Value of b is " + b);
+b = true;
+console.log("Value of b is " + b);
---
+++ Typescript/Day03/demo1.ts
@@ -0,0 +1,75 @@
+
+// console.log('Hello World');
+var a1,b1;
+
+var a$b;
+
+// int a;
+//  var ph:phone;
+
+//data types
+// number(int,long,short,double,float),string("",''),boolean(true/false)
+// any (number,string,boolean), void(netural it does point any thing)
+//misec. null & undefined (value as well as datatype)
+
+
+var a:number=10; //forward declaration & defination
+// console.log("Value of a is "+a);
+// console.log(`value of a is => ${a} `);
+// var b:number;
+// console.log('Value of b is '+b);
+// var str:string;
+// console.log('Str Value is => '+str)
+// str="s";
+// console.log('Str Value is => '+str);
+
+var b:any;
+b=2.5;
+console.log("Value of b is "+b);
+b='Sumit';
+console.log("Value of b is "+b);
+b=true;
+console.log("Value of b is "+b);
+
+var c;
+
+//type assertion
+var c:any;
+
+//u can use type assertion in 2 ways
+// 1 angle bracket syntax
+// 2 as syntax
+
+// 1 angle bracket syntax
+
+
+var temp=(<string> c)
+// temp.
+// (<string> c).
+
+// 2 as syntax
+
+var temp2=(c as string)
+// temp2.
+
+
+//operators
+//Airthemathic => +,-,/,*,%
+//Bitwise => &,|, ~, ^,<<,>>
+//Logical => &&, ||, 
+//assignment => =,+=,-=
+//relational=> <=,>=,!=,==(only checks tha value),===(it checks value as well as data type)
+//  a===b strongly equality or strictly equality 
+//unaray operator => ++,--,pre/post inc/dec a++,--a
+//ternary => condition?expression1:expression2;
+
+// var c2:number=2;
+// var c1:string='2'
+
+// if(c2===c1){
+//     console.log("ndsvcmZBDv ")
+// }
+
+
+
+
